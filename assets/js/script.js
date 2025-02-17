b4aa4a4383ce41c13d51d5cb30d1aee664ccba24
MBOYADAISY <92984076+MBOYADAISY@users.noreply.github.com>
2025-02-17 10:08:04 -0800

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,11 +70,13 @@ function populateProjectDropdown(category) {
 }
 
 // Toggle dropdowns on click
-selectCategory.addEventListener("click", function () {
+selectCategory.addEventListener("click", function (event) {
+  event.stopPropagation();
   elementToggleFunc(this);
 });
 
-selectProject.addEventListener("click", function () {
+selectProject.addEventListener("click", function (event) {
+  event.stopPropagation();
   elementToggleFunc(this);
 });
 
@@ -201,15 +203,16 @@ const pages = document.querySelectorAll("[data-page]");
 
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
-  navigationLinks[i].addEventListener("click", function () {
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+  navigationLinks[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    for (let j = 0; j < pages.length; j++) {
+      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+        pages[j].classList.add("active");
+        this.classList.add("active");
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
       }
     }
   });
